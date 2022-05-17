b5faca8fcd030bb5631d2c1bd179a03e46e744d4
Adroaldo <hygino@msn.com>
2022-05-17 19:39:23 -0300

    Find category by id, exception handling

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,14 +1,18 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exception.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,11 +20,22 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 
+	@Autowired
+	private ModelMapper mapper;
+
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		var list = categoryRepository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		// list.forEach(x -> dtoList.add(new CategoryDTO(x)));
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = categoryRepository.findById(id);
+		Category entity = obj.orElseThrow(()->new EntityNotFoundException("Entity not found"));
+		return mapper.map(entity, CategoryDTO.class);
+		//return new CategoryDTO(entity);
 	}
 
 }
