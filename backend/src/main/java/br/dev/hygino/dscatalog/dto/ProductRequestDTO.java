78221a1607f8d504ca8926ef6532f2875ce7ef91
Adroaldo <hygino@msn.com>
2024-12-12 13:54:21 -0300

    ProductRequestDTO validation

--- backend/src/main/java/br/dev/hygino/dscatalog/dto/ProductRequestDTO.java
+++ backend/src/main/java/br/dev/hygino/dscatalog/dto/ProductRequestDTO.java
@@ -4,14 +4,16 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Set;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record ProductRequestDTO(
-                String name,
-                String description,
-                Double price,
-                String imgUrl,
-                Instant date,
-                @Size(min = 1, message = "Produto deve ter pelo menos uma categoria!") Set<Long> categories)
-                implements Serializable {
+        @NotBlank String name,
+        String description,
+        @NotNull Double price,
+        String imgUrl,
+        @NotNull Instant date,
+        @Size(min = 1, message = "Produto deve ter pelo menos uma categoria!") Set<Long> categories)
+        implements Serializable {
 }
