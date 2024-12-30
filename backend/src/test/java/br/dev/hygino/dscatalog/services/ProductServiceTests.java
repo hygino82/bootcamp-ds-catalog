b12a19f607b43673637db6722b30c514d7a8309e
Adroaldo <hygino@msn.com>
2024-12-30 20:26:07 -0300

    ProductServiceTests: findAllPaged

--- backend/src/test/java/br/dev/hygino/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/br/dev/hygino/dscatalog/services/ProductServiceTests.java
@@ -1,22 +1,31 @@
 package br.dev.hygino.dscatalog.services;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import br.dev.hygino.dscatalog.dto.ProductRequestDTO;
+import br.dev.hygino.dscatalog.entities.Product;
+import br.dev.hygino.dscatalog.repositories.CategoryRepository;
+import br.dev.hygino.dscatalog.repositories.ProductRepository;
 import br.dev.hygino.dscatalog.services.exceptions.DatabaseException;
+import br.dev.hygino.dscatalog.services.exceptions.ResourceNotFoundException;
+import br.dev.hygino.dscatalog.tests.Factory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import br.dev.hygino.dscatalog.repositories.CategoryRepository;
-import br.dev.hygino.dscatalog.repositories.ProductRepository;
-import br.dev.hygino.dscatalog.services.exceptions.ResourceNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -24,12 +33,19 @@ public class ProductServiceTests {
     private long existingId;
     private long nonExistingId;
     private long dependentId;
+    private Pageable pageable;
+    private Product product;
+    private ProductRequestDTO requestDTO;
 
     @BeforeEach
     public void setUp() throws Exception {
         existingId = 1L;
         dependentId = 25L;
         nonExistingId = 1000L;
+        pageable = PageRequest.of(0, 10);
+        final PageImpl<Product> page = new PageImpl<>(Factory.createProductList());
+        product = Factory.createProduct();
+        requestDTO = Factory.createProductRequest();
 
         Mockito.doNothing().when(productRepository).deleteById(existingId);
         Mockito.doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
@@ -37,6 +53,15 @@ public class ProductServiceTests {
         Mockito.when(productRepository.existsById(existingId)).thenReturn(true);
         Mockito.when(productRepository.existsById(nonExistingId)).thenReturn(false);
         Mockito.when(productRepository.existsById(dependentId)).thenReturn(true);
+
+        Mockito.when(productRepository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+
+        Mockito.when(productRepository.save(ArgumentMatchers.any())).thenReturn(product);
+
+        Mockito.when(categoryRepository.findAllById(Set.of(2L))).thenReturn(List.of(Factory.createCategory()));
+
+        Mockito.when(productRepository.findById(existingId)).thenReturn(Optional.of(product));
+        Mockito.when(productRepository.findById(nonExistingId)).thenReturn(Optional.empty());
     }
 
     @InjectMocks
@@ -65,5 +90,18 @@ public class ProductServiceTests {
     @DisplayName("Delete deve lançar DatabaseException quando o id não existir")
     public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
         assertThrows(DatabaseException.class, () -> service.delete(dependentId));
+    }
+
+    @Test
+    @DisplayName("FindAllPaged deve retornar uma página com três elementos")
+    public void findAllPagedShouldReturnPageWith3Elements() {
+        final var result = service.findAll(pageable);
+        assertNotNull(result);
+        assertEquals(3, result.getTotalElements());
+        assertEquals("Phone", result.getContent().get(0).name());
+        assertEquals("TV", result.getContent().get(1).name());
+        assertEquals("Radio", result.getContent().get(2).name());
+
+        Mockito.verify(productRepository, Mockito.times(1)).findAll(pageable);
     }
 }
--- backend/src/test/java/br/dev/hygino/dscatalog/tests/Factory.java
+++ backend/src/test/java/br/dev/hygino/dscatalog/tests/Factory.java
@@ -1,25 +1,72 @@
 package br.dev.hygino.dscatalog.tests;
 
-import java.time.Instant;
-
 import br.dev.hygino.dscatalog.dto.ProductDTO;
+import br.dev.hygino.dscatalog.dto.ProductRequestDTO;
 import br.dev.hygino.dscatalog.entities.Category;
 import br.dev.hygino.dscatalog.entities.Product;
+import org.springframework.data.domain.PageImpl;
+
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 public class Factory {
-	public static Product createProduct() {
-		Product product = new Product(
-				1L, "Phone", 
-				"Good Phone", 
-				"https://img.com/img.png", 800.0,
-				Instant.parse("2024-12-18T07:12:00Z"));
-		product.getCategories().add(new Category(2L, "Eletrônicos"));
+    public static Category createCategory() {
+        return new Category(2L, "Eletrônicos");
+    }
 
-		return product;
-	}
+    public static Product createProduct() {
+        Product product = new Product(
+                1L, "Phone",
+                "Good Phone",
+                "https://img.com/img.png", 800.0,
+                Instant.parse("2024-12-18T07:12:00Z"));
+        product.getCategories().add(new Category(2L, "Eletrônicos"));
 
-	public static ProductDTO createProductDTO() {
-		Product product = createProduct();
-		return new ProductDTO(product, true);
-	}
+        return product;
+    }
+
+    public static ProductDTO createProductDTO() {
+        Product product = createProduct();
+        return new ProductDTO(product, true);
+    }
+
+    public static List<Product> createProductList() {
+        final Product product1 = new Product(
+                1L,
+                "Phone",
+                "Good Phone",
+                "https://img.com/img.png", 800.0,
+                Instant.parse("2024-12-18T07:12:00Z"));
+        product1.getCategories().add(createCategory());
+
+        final Product product2 = new Product(
+                2L,
+                "TV",
+                "Smart TV",
+                "https://img.com/tv.png", 900.45,
+                Instant.parse("2024-12-28T07:12:00Z"));
+        product2.getCategories().add(createCategory());
+
+        final Product product3 = new Product(
+                3L,
+                "Radio",
+                "Smart TV",
+                "https://img.com/radio.png", 45.0,
+                Instant.parse("2024-12-30T07:12:00Z"));
+        product3.getCategories().add(createCategory());
+
+        return Arrays.asList(product1, product2, product3);
+    }
+
+    public static ProductRequestDTO createProductRequest() {
+        return new ProductRequestDTO(
+                "Phone",
+                "Good Phone", 800.0,
+                "https://img.com/img.png",
+                Instant.parse("2024-12-18T07:12:00Z")
+                , Set.of(2L));
+
+    }
 }
