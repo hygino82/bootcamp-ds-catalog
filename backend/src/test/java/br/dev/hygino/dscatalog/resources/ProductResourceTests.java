3f1788108820991eb3fa8404a8f650ab04746c64
Adroaldo <hygino@msn.com>
2024-12-31 07:24:59 -0300

    Melhorando a legibilidade e negociação de conteúdo

--- backend/src/test/java/br/dev/hygino/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/br/dev/hygino/dscatalog/resources/ProductResourceTests.java
@@ -14,7 +14,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
@@ -48,6 +50,8 @@ public class ProductResourceTests {
     @Test
     @DisplayName("FindAllPaged deve retornar uma página")
     public void findAllPagedShouldReturnPage() throws Exception {
-        mockMvc.perform(get("/products")).andExpect(status().isOk());
+        final ResultActions result = mockMvc.perform(get("/products").accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+        //Legibilidade e negociação de conteúdo, dividindo as chamadas
     }
 }
