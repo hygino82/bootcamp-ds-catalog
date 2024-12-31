7391bd9d4f3fac8a50f86273d17ba62d64ab86e3
Adroaldo <hygino@msn.com>
2024-12-31 07:52:31 -0300

    Testando o findById

--- backend/src/main/java/br/dev/hygino/dscatalog/resources/exceptions/StandardError.java
+++ backend/src/main/java/br/dev/hygino/dscatalog/resources/exceptions/StandardError.java
@@ -1,9 +1,11 @@
 package br.dev.hygino.dscatalog.resources.exceptions;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
 
 public class StandardError implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private Instant timestamp;
--- backend/src/test/java/br/dev/hygino/dscatalog/resources/ProductResourceTests.java
+++ backend/src/test/java/br/dev/hygino/dscatalog/resources/ProductResourceTests.java
@@ -2,6 +2,7 @@ package br.dev.hygino.dscatalog.resources;
 
 import br.dev.hygino.dscatalog.dto.ProductDTO;
 import br.dev.hygino.dscatalog.services.ProductService;
+import br.dev.hygino.dscatalog.services.exceptions.ResourceNotFoundException;
 import br.dev.hygino.dscatalog.tests.Factory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -21,6 +22,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductResource.class)
@@ -45,13 +47,47 @@ public class ProductResourceTests {
         nonExistingId = 25L;
 
         Mockito.when(service.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+
+        Mockito.when(service.findById(existingId)).thenReturn(productDTO);
+        Mockito.when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
     }
 
     @Test
     @DisplayName("FindAllPaged deve retornar uma página")
     public void findAllPagedShouldReturnPage() throws Exception {
-        final ResultActions result = mockMvc.perform(get("/products").accept(MediaType.APPLICATION_JSON));
+        final ResultActions result = mockMvc.perform(
+                get("/products")
+                        .accept(MediaType.APPLICATION_JSON));
         result.andExpect(status().isOk());
-        //Legibilidade e negociação de conteúdo, dividindo as chamadas
+    }
+
+    @Test
+    @DisplayName("FindById deve retornar um produto com resposta OK")
+    public void findByIdShouldReturnProductWhenIdExists() throws Exception {
+        final var result = mockMvc.perform(
+                get("/products/{id}", existingId)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+
+        result.andExpect(jsonPath("$.id").exists());
+        result.andExpect(jsonPath("$.name").exists());
+        result.andExpect(jsonPath("$.description").exists());
+        result.andExpect(jsonPath("$.date").exists());
+    }
+
+    @Test
+    @DisplayName("FindById deve retornar NOT_FOUND")
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
+        final var result = mockMvc.perform(
+                get("/products/{id}", nonExistingId)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isNotFound());
+
+        result.andExpect(jsonPath("$.timestamp").exists());
+        result.andExpect(jsonPath("$.status").exists());
+        result.andExpect(jsonPath("$.error").exists());
+        result.andExpect(jsonPath("$.path").exists());
     }
 }
