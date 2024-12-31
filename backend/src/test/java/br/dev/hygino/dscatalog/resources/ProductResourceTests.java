c749c9dfde1b0267232f87f544ce105bf1aebd5f
Adroaldo <hygino@msn.com>
2024-12-30 21:56:22 -0300

    ProductResourceTests: findAll

---
+++ backend/src/test/java/br/dev/hygino/dscatalog/resources/ProductResourceTests.java
@@ -0,0 +1,53 @@
+package br.dev.hygino.dscatalog.resources;
+
+import br.dev.hygino.dscatalog.dto.ProductDTO;
+import br.dev.hygino.dscatalog.services.ProductService;
+import br.dev.hygino.dscatalog.tests.Factory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductResource.class)
+public class ProductResourceTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService service;
+
+    private long existingId;
+    private long nonExistingId;
+    private ProductDTO productDTO;
+    private PageImpl<ProductDTO> page;
+
+    @BeforeEach
+    public void setUp() {
+        productDTO = Factory.createProductDTO();
+        page = new PageImpl<>(List.of(productDTO));
+        existingId = 1L;
+        nonExistingId = 25L;
+
+        Mockito.when(service.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+    }
+
+    @Test
+    @DisplayName("FindAllPaged deve retornar uma página")
+    public void findAllPagedShouldReturnPage() throws Exception {
+        mockMvc.perform(get("/products")).andExpect(status().isOk());
+    }
+}
